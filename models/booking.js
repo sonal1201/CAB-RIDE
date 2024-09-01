85ceb0349b46f44b8a0c2b378f0647b8bf41723c
Sonal Singh <sonal1201kumar@gmail.com>
2024-09-01 23:43:49 +0530

    Feat: Added models/user and model/bookings

---
+++ models/booking.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    passenger: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    driver: { type: mongoose.Schema.Types.ObjectId, ref: 'user', default: null },
+    source: {
+        latitude: {
+            type: Number
+        },
+        logitude: {
+            type: Number
+        }
+    },
+    fare: { type: Number },
+    status: { type: String, enum: ['Confirmed', 'Pending', 'Compeleted', 'Cancelled'], default: 'Pending' },
+    rating: { type: Number },
+    feedback: { type: String }
+
+})
+
+const Booking = mongoose.model('Bookings', bookingSchema);
+
+module.exports = Booking
---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcryptjs');
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    role: { type: String, enum: ['driver', 'passenger'] },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            default: 'Point'
+        },
+        coordinates: {
+            type: [Number],
+            default: [0, 0]
+        }
+    }
+
+})
+
+// This is the pre save middleware that runs before a docs save to db
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password)
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
